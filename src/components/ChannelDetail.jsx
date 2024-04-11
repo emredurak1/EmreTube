089dca44e3a17420d98cc809608867ac0d312f0c
emre <emredurak543@gmail.com>
2024-04-11 17:14:25 +0300

    will be upgraded

---
+++ src/components/ChannelDetail.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { Videos, ChannelCard } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
+const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) =>
+      setChannelDetail(data?.items[0])
+    );
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then(
+      (data) => setVideos(data?.items)
+    );
+  }, [id]);
+
+  return (
+    <Box minHeight="95vh">
+      <Box>
+        <div
+          style={{
+            background:
+              "linear-gradient(90deg, rgba(207,5,33,0.9109768907563025) 35%, rgba(103,20,20,1) 69%, rgba(111,0,19,0.9249824929971989) 100%)",
+            zIndex: 10,
+            height: "300px",
+          }}
+        />
+
+        <ChannelCard marginTop="-110px" channelDetail={channelDetail} />
+      </Box>
+      <Box display="flex" p="2">
+        <Box sx={{ mr: { sm: "100px" } }} />
+        <Videos videos={videos} />
+      </Box>
+    </Box>
+  );
+};
+
+export default ChannelDetail;
